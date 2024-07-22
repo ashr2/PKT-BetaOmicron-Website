8c12667436ab6bff2973bd5a4bc7b89cd38d8014
ashrdev <ashrdev@gmail.com>
2024-07-22 18:00:43 -0400

    Modified links in BrowserRouter

--- main/src/App.js
+++ main/src/App.js
@@ -5,17 +5,19 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home/Home';
 import Alumni from './components/Alumni/Alumni';
+import Rush from './components/Rush/Rush';
+
 function App() {
   return (
     <div className="App">
       <CustomNavbar/>
       <BrowserRouter>
         <Routes>
-        <Route path="home" element={<h1>Home</h1>}/>
-        <Route path="rush" element={<h1>Rush</h1>}/>
-        <Route path="social" element={<h1>Social</h1>}/>
-        <Route path="alumni" element={<Alumni/>}/>
-        <Route path="academics" element={<h1>Academics</h1>}/>
+          <Route path="home" element={<h1>Home</h1>}/>
+          <Route path="rush" element={<Rush/>}/>
+          <Route path="social" element={<h1>Social</h1>}/>
+          <Route path="alumni" element={<Alumni/>}/>
+          <Route path="academics" element={<h1>Academics</h1>}/>
         </Routes>
       </BrowserRouter>
     </div>
