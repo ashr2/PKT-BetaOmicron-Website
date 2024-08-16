e59c760bc511a34068e52376d34f0788369298dc
ashwarau <ashwarau@amazon.com>
2024-08-16 09:06:25 -0400

    changes

--- main/src/components/CustomNavbar/CustomNavbar.js
+++ main/src/components/CustomNavbar/CustomNavbar.js
@@ -1,26 +1,43 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
 import './CustomNavbar.css'
-import logo from './logo1.png'
 
 function CustomNavbar() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <Navbar expand="lg" className="navbar">
-      <Container>
-        <Navbar.Brand href="home"><img src={logo} height={'32px'} width={'65px'} /></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto" >
-            <Nav.Link href="rush"><b>Rush</b></Nav.Link>
-            <Nav.Link href="social"><b>Social</b></Nav.Link>
-            <Nav.Link href="alumni"><b>Professional & Alumni</b></Nav.Link>
-            <Nav.Link href="academics"><b>Academics</b></Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar expand="lg">
+          <Navbar.Brand href="#home" onClick={handleShow}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" class="bi bi-list" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+          </svg>
+          </Navbar.Brand>
+      </Navbar>
+
+      <Modal show={show} onHide={handleClose}>
+        <Container>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="flex-column">
+              <a href="home">Home</a><br/>
+              <a href="rush">Rush</a><br/>
+              <a href="social">Social</a><br/>
+              <a href="alumni">Alumni</a><br/>
+              <a href="academics">Academics</a><br/>
+          </Modal.Body>
+          <Modal.Footer>
+            <h1 onClick={handleClose}> Close </h1>
+          </Modal.Footer>
+        </Container>
+      </Modal>
+    </>
   );
 }
 
--- main/src/components/Rush/RushCard.js
+++ main/src/components/Rush/RushCard.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './RushCard.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
 function RushCard({ info, imagePosition }) {
   const cardStyle = {
@@ -22,15 +20,32 @@ function RushCard({ info, imagePosition }) {
   };
 
   const leftAligned = (
-    <>
-      <Col xs={12} md={4} style={{ padding: '0' }}>
-        <img src={info.imgSrc} alt={info.header} style={imgStyle} />
-      </Col>
-      <Col xs={12} md={8} style={{ padding: '0' }}>
-        <h1>{info.header}</h1>
-        <p>{info.description}</p>
-      </Col>
-    </>
+    <Container fluid style={{ backgroundColor: '#A3392F', color: 'white', padding: '0px 0' }}>
+          <Row>
+            <Col md={6} className="d-flex align-items-center justify-content-center">
+              <img
+                src={info.imgSrc}
+                alt="Group of people"
+                style={{ width: '100%', height: 'auto', maxHeight: '500px', display: 'block' }}
+              />
+            </Col>
+            <Col md={6} className="d-flex align-items-center">
+              <Card style={{ backgroundColor: '#A3392F', border: 'none', color: 'white', padding: '0 40px' }}>
+                <Card.Body>
+                  <Card.Title style={{ fontSize: '2rem', fontWeight: 'bold', marginBottom: '20px', alignItems: 'left'}}>
+                    Academics
+                  </Card.Title>
+                  <Card.Text style={{ fontSize: '1.25rem', lineHeight: '1.5' }}>
+                    We take our majors seriously and have multiple academic events throughout the year in order to maintain our average 3.45 GPA. We get the rigor that comes with being an Engineer, and being part of our brotherhood means that you always have someone to reach out to if you're falling behind in any of your classes or need someone to study with.
+                  </Card.Text>
+                  <Button variant="outline-light" style={{ marginTop: '20px', padding: '10px 20px' }}>
+                    LEARN MORE
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
   );
 
   const rightAligned = (
